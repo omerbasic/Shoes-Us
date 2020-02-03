62562c81342d28a7e16837d6744c8924bb922a9d
Michelle Lanaro <michelle.lanaro@medieinstitutet.se>
2020-02-03 14:34:27 +0100

    Grund style och index.php content

---
+++ SHOESUS/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shoes Us - Indez</title>
+    <link rel="stylesheet" href="./style/mainStyle.css">
+    <link href="https://fonts.googleapis.com/css?family=Rock+Salt&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="headTitle">
+            <h1 class="textTitle">Shoes US</h1>
+        </div>
+    </header>
+
+    <nav> 
+        <div class="dropdown">
+            <button class="dropBtn">Dam
+            </button>
+            <div class="dropdownContent">
+                <a href="#">Stövlar</a>
+                <a href="#">Stövlar</a>
+                <a href="#">Stövlar</a>
+                <a href="#">Stövlar</a>
+            </div>
+        </div>  
+
+        <div class="dropdown">
+        <button class="dropBtn">Herr</button>
+            <div class="dropdownContent">
+                <a href="#">Sneakers</a>
+                <a href="#">Sneakers</a>
+                <a href="#">Sneakers</a>
+                <a href="#">Sneakers</a>
+            </div>
+        </div>
+
+        <div class="loginCartWrap">
+            <div class="loginText">
+                Login
+            </div>
+
+            <div class="myPageText">
+                My page
+            </div>
+
+            <div class="cartField">
+                Cart (0)
+            </div>
+        </div>
+    </nav>
+    
+</body>
+<footer>
+
+    <div class="newsletterField">
+        Prenumerera på vårt nyhetsbrev: <input type="text" class="newsletterInput">
+    </div>
+
+    <div class="footWrap">
+        <p class="contactUs">Kontakta Oss</p>
+        <p class="clientService">Kundtjänst</p>
+        <p class="beMember">Bli medlem</p>
+    </div>
+
+</footer>
+</html>
