754465b1282f1e8f049f68d9c4d7ee0017a4d3ae
Michelle Lanaro <michelle.lanaro@medieinstitutet.se>
2020-02-17 14:21:18 +0100

    Fixed navbar on: login; index

--- index.php
+++ index.php
@@ -13,11 +13,7 @@
     <!-- Link to get th right font -->
     <link href="https://fonts.googleapis.com/css?family=Rock+Salt&display=swap" rel="stylesheet">
     <!-- Link to use Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/e8127072bf.js" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
     <!-- Link to the handler.js -->
     <script defer type="module" src="./handler.js"></script>
 
@@ -66,16 +62,13 @@
         </div>
     </nav>
    
-    <div class="article">
+            <div id="allProducts">
+                <div class="menFemImg">
+                    <a href="femShoes.php"><img src="./img/damskor.jpg" alt="FemShoes" id="damBild"></a>
+                    <a href="menShoes.php"><img src="./img/herrskor.jpg" alt="MenShoes" href="menShoes.php" id="herrBild"></a>
+                </div>
+            </div>    
         
-    </div>
-
-    <div id="allProducts">
-        <div class="menFemImg">
-            <a href="femShoes.php"><img src="./img/damskor.jpg" alt="FemShoes" id="damBild"></a>
-            <a href="menShoes.php"><img src="./img/herrskor.jpg" alt="MenShoes" href="menShoes.php" id="herrBild"></a>
-        </div>
-    </div>
 
     <footer>
         <br> <br>
