930ff30d614141f20288866ac961d836a1b2da78
Michelle Lanaro <michelle.lanaro@medieinstitutet.se>
2020-02-17 14:22:27 +0100

    Fixed navabr

--- myPage.php
+++ myPage.php
@@ -6,12 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ShoesUs - Hemsida</title> 
     <link rel="stylesheet" href="./style/mainStyle.css">
-    <!-- Link to use Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/css?family=Rock+Salt&display=swap" rel="stylesheet">
     <script src="handler.js"></script>
 </head>
 <body>
@@ -23,49 +17,21 @@
     
 
     <nav> 
+        <div class="manWomanField">
+            <div class="dam">
+                <a href="#" id="womenShoes">Dam</a>
+            </div>
 
-        <div class="mobileScreenMenu">
-            <div class="RespDropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Meny
-                </button>
+            <div class="herr">
+                <a href="#" id="menShoes">Herr</a>
+            </div>
 
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="sales.php">% REA %</a>
-                    <hr>
-                    <a class="dropdown-item" href="myPage.php">MITT KONTO</a>
-                    <hr>
-                    <a class="dropdown-item" href="login.php">Inloggning</a>
-                    <a class="dropdown-item" href="myPage.php">Mina sidor</a>
-                    <a class="dropdown-item" href="cart.php">Min kundvagn</a>
-                </div>
+            <div class="rea">
+                <a href="#" id="sale">Rea</a>
             </div>
         </div>
-        
 
-    <div class="largeScreenMenu">
-        <div class="manWomanField">
-        <div class="dropdown" id="herr">
-            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                HERR
-            </a>
 
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <div class="categoryListDropDown" id="menShoes">Herr</div>
-              
-            </div>
-        </div>  
-
-        <div class="dropdown" id="dam">
-            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                DAM
-            </a>
-
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <div class="categoryListDropDown" id="womenShoes">Dam</div>
-            </div>
-        </div> 
-        </div> 
      
 
         <div class="loginCartWrap">
@@ -81,12 +47,12 @@
                 <div class="numberOfItemsInCart">(...)</div>
                 <a href="cart.php"><img src="./img/CartIcon.png" style="width:20px;height:auto;text-align:right;"></a>
             </div>
+   
         </div>
-    </div>
     </nav>
    
-    <div class="article">
-      <h1 style="color: #333333;">Mina beställningar</h1>
+    <div class="myPageBody">
+      <h1 style="color: #333333; text-align: center;">Mina beställningar</h1>
     </div>
     
     
