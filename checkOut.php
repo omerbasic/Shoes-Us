087ffab4fd6ad6bb2bdbc11dfeb382a607f552ca
Michelle Lanaro <michelle.lanaro@medieinstitutet.se>
2020-02-19 10:35:56 +0100

    CheckOutPage fixat style

--- checkOut.php
+++ checkOut.php
@@ -8,76 +8,34 @@
     <link rel="stylesheet" href="./style/mainStyle.css">
     <link rel="stylesheet" href="./style/checkOutStyle.css">
     <!-- Link to get th right font -->
-    <link href="https://fonts.googleapis.com/css?family=Rock+Salt&display=swap" rel="stylesheet">
-    <!-- Link to use Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Rock+Salt&display=swap" rel="stylesheet">    
     <!-- Link to the handler.js -->
     <script defer type ="module" src="./checkOutHandler.js"></script>
 </head>
 <body>
 <header>
         <div class="headTitle">
-            <h1 class="textTitle" onclick="toIndex()">Shoes US</h1>
+            <h1 class="textTitle">Shoes US</h1>
         </div>
     </header>
     
 
     <nav> 
+        <div class="manWomanField">
+            <div class="dam">
+                <a href="#" id="womenShoes">Dam</a>
+            </div>
 
-        <div class="mobileScreenMenu">
-            <div class="RespDropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Meny
-                </button>
+            <div class="herr">
+                <a href="#" id="menShoes">Herr</a>
+            </div>
 
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="sales.php">% REA %</a>
-                    <hr>
-                    <a class="dropdown-item" href="myPage.php">MITT KONTO</a>
-                    <hr>
-                    <a class="dropdown-item" href="login.php">Inloggning</a>
-                    <a class="dropdown-item" href="myPage.php">Mina sidor</a>
-                    <a class="dropdown-item" href="cart.php">Min kundvagn</a>
-                </div>
+            <div class="rea">
+                <a href="#" id="sale">Rea</a>
             </div>
         </div>
-        
 
-    <div class="largeScreenMenu">
-        <div class="manWomanField">
-        <div class="dropdown" id="herr">
-            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                HERR
-            </a>
 
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <a class="categoryListDropDown" href="menShoes.php">HERR (Alla)</a> <br>
-                <hr>
-                <a class="categoryListDropDown" href="#">Boots</a> <br>
-                <a class="categoryListDropDown" href="#">Sneakers</a> <br>
-                <a class="categoryListDropDown" href="#">Lågskor</a> 
-            </div>
-        </div>  
-
-        <div class="dropdown" id="dam">
-            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                DAM
-            </a>
-
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <a class="categoryListDropDown" href="femShoes.php">DAM (Alla)</a> <br>
-                <hr>
-                <a class="categoryListDropDown" href="#">Boots</a> <br>
-                <a class="categoryListDropDown" href="#">Sneakers</a> <br>
-                <a class="categoryListDropDown" href="#">Lågskor</a> <br>
-                <a class="categoryListDropDown" href="#">Pumps</a> <br>
-                <a class="categoryListDropDown" href="#">Stövlar</a> 
-            </div>
-        </div> 
-        </div> 
      
 
         <div class="loginCartWrap">
@@ -93,12 +51,11 @@
                 <div class="numberOfItemsInCart">(...)</div>
                 <a href="cart.php"><img src="./img/CartIcon.png" style="width:20px;height:auto;text-align:right;"></a>
             </div>
+   
         </div>
-    </div>
     </nav>
 
     
-    <div class="article">
 
         <div class="categoryTitle">
             <h1>Check out</h1>
@@ -130,7 +87,7 @@
         </form>
         </div>
 
-    </div>
+    
     
     
     <footer>
