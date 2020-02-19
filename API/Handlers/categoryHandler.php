991dd057528f5880117c9e57e8bd8560446bf380
fridaedda <frida.baldvinsdottir@gmail.com>
2020-02-19 14:12:13 +0100

    Visa alla produkter

--- API/Handlers/categoryHandler.php
+++ API/Handlers/categoryHandler.php
@@ -14,12 +14,28 @@ function getAll() {
     }
     return $result; 
 }
+
 function getSpecific($specificCategory) {
     include_once('./../Class/database.php');
     $database = new Database();
 
     $query = $database->connection->prepare('SELECT * FROM categorydetails JOIN product ON categorydetails.productID = product.productID WHERE categoryID = :myCategoryID;');
     $query->execute(array(':myCategoryID' => $specificCategory));
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    if (empty($result)) {
+        throw new exception('No category found', 404);
+        exit;
+    }
+    return $result; 
+}
+
+function getAllProducts() {
+    include_once('./../Class/database.php');
+    $database = new Database();
+
+    $query = $database->connection->prepare('SELECT * FROM product;');
+    $query->execute();
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
 
     if (empty($result)) {
