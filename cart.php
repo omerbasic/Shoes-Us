4c38c655bc510a88dc984d032c398f715415eee7
Michelle Lanaro <michelle.lanaro@medieinstitutet.se>
2020-02-19 14:44:21 +0100

    fixed styling in cart

--- cart.php
+++ cart.php
@@ -7,14 +7,10 @@
     <title>ShoesUs - Hemsida</title> 
     <link rel="stylesheet" href="./style/mainStyle.css">
     <link rel="stylesheet" href="./style/cartStyle.css">
-    <!-- Link to use Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/e8127072bf.js" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css?family=Rock+Salt&display=swap" rel="stylesheet">
     <script defer type="module" src="cartHandler.js"></script>
+    
 </head>
 <body>
 <header>
@@ -25,7 +21,8 @@
     
 
 <nav> 
-    <div id="toIndex"><a href="index.php">Startsidan</a></div>    
+    <div id="toIndex"><a href="index.php">Startsidan</a></div>   
+   
 
     <div class="loginCartWrap">
         <div class="loginText">
--- checkOut.php
+++ checkOut.php
@@ -25,15 +25,15 @@
     <nav> 
         <div class="manWomanField">
             <div class="dam">
-                <a href="#" id="womenShoes">Dam</a>
+                <a href="index.php" id="womenShoes">Dam</a>
             </div>
 
             <div class="herr">
-                <a href="#" id="menShoes">Herr</a>
+                <a href="index.php" id="menShoes">Herr</a>
             </div>
 
             <div class="rea">
-                <a href="#" id="sale">Rea</a>
+                <a href="index.php" id="sale">Rea</a>
             </div>
         </div>
 
