e4506b1da8b50289266bb202d9c81514ff73bce8
juliamattson <julia.mattson@medieinstitutet.se>
2020-02-21 15:15:37 +0100

    Funktion för att räkna ut totalbeloppet av cart

--- cartHandler.js
+++ cartHandler.js
@@ -3,47 +3,52 @@ function getCart() {
   }
 
 function showProductsInCart() {
-    
     const showCartProducts = document.getElementById("productsInCart")
-        showCartProducts.innerHTML = "" 
+    showCartProducts.innerHTML = "" 
 
-        for (let i = 0; i < getCart().length; i++) {
-            const selectedProduct = getCart()[i]
-            
-            const productDiv = document.createElement("div")
-            productDiv.classList = "productDiv"
+    for (let i = 0; i < getCart().length; i++) {
+        const selectedProduct = getCart()[i]
+        
+        const productDiv = document.createElement("div")
+        productDiv.classList = "productDiv"
 
-            const image = document.createElement("img")
-            image.setAttribute("src", "./img/products/" + selectedProduct.img_path)
-            image.classList = "productImage"
-            // Kundvagns-ikon i knappen
-           
-            const title = document.createElement("p")
-            title.innerHTML = selectedProduct.name
-            const price = document.createElement("p") 
-            price.innerHTML = selectedProduct.price - selectedProduct.discount + " kr"
+        const image = document.createElement("img")
+        image.setAttribute("src", "./img/products/" + selectedProduct.img_path)
+        image.classList = "productImage"
+        // Kundvagns-ikon i knappen
+        
+        const title = document.createElement("p")
+        title.innerHTML = selectedProduct.name
+        const price = document.createElement("p") 
+        price.innerHTML = selectedProduct.price - selectedProduct.discount + " kr"
 
-            const deleteProductBtn = document.createElement("button")
-            deleteProductBtn.classList = "deleteProductBtn"
-            deleteProductBtn.innerHTML = "Ta bort"
-            deleteProductBtn.num = i;
-            deleteProductBtn.addEventListener("click", function(){
-                removeProductFromCart(this.num)
-                numberOfProductsInCart()
-            })
+        const deleteProductBtn = document.createElement("button")
+        deleteProductBtn.classList = "deleteProductBtn"
+        deleteProductBtn.innerHTML = "Ta bort"
+        deleteProductBtn.num = i;
+        deleteProductBtn.addEventListener("click", function(){
+            removeProductFromCart(this.num)
+            numberOfProductsInCart()
+        })
 
-            productDiv.append(image)
-            productDiv.append(title)
-            productDiv.append(price)
-            if(selectedProduct.discount != 0) {
-                const discount = document.createElement("p")
-                discount.innerHTML = "10 %"
-                discount.id = "discount"
-                productDiv.append(discount)
-            }
-            productDiv.append(deleteProductBtn)
-            showCartProducts.append(productDiv)            
+        productDiv.append(image)
+        productDiv.append(title)
+        productDiv.append(price)
+        if(selectedProduct.discount != 0) {
+            const discount = document.createElement("p")
+            discount.innerHTML = "10 %"
+            discount.id = "discount"
+            productDiv.append(discount)
         }
+        productDiv.append(deleteProductBtn)
+        showCartProducts.append(productDiv)    
+    }
+
+    const sumDiv = document.getElementById("goToCheckout")
+    const sum = document.createElement("p")
+    sum.classList = "sumText"
+    sum.innerText = "Totalbelopp: " + totalCart() + " kr"
+    sumDiv.appendChild(sum)
 }
 showProductsInCart()
 
@@ -61,3 +66,13 @@ function numberOfProductsInCart() {
     quantity.innerHTML = getCart.length   
 }
 numberOfProductsInCart()
+
+function totalCart(){
+    const cart = JSON.parse(localStorage.getItem("localCart"))
+    let total = 0;
+    for (var i = 0; i < cart.length; i++){
+        total += Number(cart[i].price);
+    }
+    return total
+}
+
