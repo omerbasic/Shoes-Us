44e69921eb0d0f16538d5e9fedd7de67e1633a16
Michelle Lanaro <michelle.lanaro@medieinstitutet.se>
2020-02-21 11:09:43 +0100

    Selected single shipper with Välj knapp

--- Resources/shipperResource.js
+++ Resources/shipperResource.js
@@ -25,38 +25,37 @@ export function getAllShippers() {
 
 function renderShippers(result) {  
 
-   var shippersWrap = document.getElementById("shippers");
-   var shipper = result;
-   /* var specificShipperDiv = document.createElement("div")
-   specificShipperDiv.classList = "specificShipper"
-   specificShipperDiv.innerHTML = ''
-   shippersWrap.appendChild(specificShipperDiv); */
+   let shippersWrap = document.getElementById("shippers");
+   let shipper = result;
    
-   for (var i = 0; i < shipper.length; i++) {
-    var selectedShipper = shipper[i];
+   for (let i = 0; i < shipper.length; i++) {
+    let selectedShipper = shipper[i];
 
-    var shipperDiv = document.createElement('div');
+    let shipperDiv = document.createElement('div');
         shipperDiv.classList = 'shipperDiv';
         
-        var shipperID = document.createElement('p');
+        let shipperID = document.createElement('p');
         shipperID.classList = 'shipperID';
         shipperID.innerText = selectedShipper.shipperID;
 
 
-        var name = document.createElement('p');
+        let name = document.createElement('p');
         name.classList = 'shipperName';
         name.innerText = selectedShipper.name;
 
 
-        var info = document.createElement('p')
+        let info = document.createElement('p')
         info.classList = 'shipperInfo';
         info.innerText = selectedShipper.info;
 
-        var buttonDiv = document.createElement('p')
+        let buttonDiv = document.createElement('p')
         buttonDiv.classList = 'buttonDiv'
-        var choiceBtn = document.createElement('button')
+        let choiceBtn = document.createElement('button')
         choiceBtn.classList = 'chooseShipper'
         choiceBtn.innerText = 'Välj'
+        choiceBtn.addEventListener('click', function() {
+            console.log(selectedShipper.shipperID)
+        })
         buttonDiv.appendChild(choiceBtn);
 
 
