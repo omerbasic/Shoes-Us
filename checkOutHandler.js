0697fdb06d1f29f098a87af16e3da1fe8d5a4a26
Michelle Lanaro <michelle.lanaro@medieinstitutet.se>
2020-02-25 14:24:38 +0100

    när man trycka på SLUTFÖR KÖP knapp ska kundvagnen tommas

--- checkOutHandler.js
+++ checkOutHandler.js
@@ -30,6 +30,8 @@ document.getElementById("checkoutSubmit").addEventListener("click", buttonFuncti
 function afterOrderAlert(){
     alert("Tack för ditt köp!");
     window.location.href='index.php';
+
+    localStorage.removeItem("localCart");
 }
 
 function buttonFunction(){
